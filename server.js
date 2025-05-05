f009dd659b213c74f00dc237b64abd7830bfb914
chengchang Ran <zx5794124@gmail.com>
2025-05-05 14:04:57 +0800

    bug修复，跨域请求

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
-const cors = require('cors');
 const SSE = require('express-sse');
 require('dotenv').config(); // Load .env for local testing
 
